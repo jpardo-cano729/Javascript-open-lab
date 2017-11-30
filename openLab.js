279f96388a45a5d3f60bf979ff3b2a79e9c58149
Unknown <jpardo-cano729@west-mec.org>
2017-11-29 17:10:14 -0800

    Created Javascript function
    In javascript i created a popup prompt asking for the user to enter their name and once they do that very name is used in the html and there is a button that displays the current date and time

---
+++ openLab.js
@@ -0,0 +1,8 @@
+function Button() {
+  document.getElementById('text').innerHTML =" Todays date and time is " + Date();
+}
+
+function box(){
+  var name1 = prompt("please enter your name");
+  document.getElementById('name').innerHTML = "Hello there " + name1 + "! How are you today"
+}
